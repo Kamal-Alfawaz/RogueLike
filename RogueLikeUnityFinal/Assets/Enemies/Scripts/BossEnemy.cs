34aeb3b4b9cc7eff3785453975649e28df359efb
Kamal-Alfawaz <kamal.alfawaz@outlook.com>
2023-12-30 01:43:36 +0000

    testing shaders

--- RogueLikeUnityFinal/Assets/Enemies/Scripts/BossEnemy.cs
+++ RogueLikeUnityFinal/Assets/Enemies/Scripts/BossEnemy.cs
@@ -7,9 +7,8 @@ public class BossEnemy : Enemy
     public GameObject nextLevelTeleportObjectPrefab;
     public GameObject currentInteractableObject;
 
-    // This is your custom method for the BossEnemy
-    public void SpawnToNextLevel(){            
-        Instantiate(nextLevelTeleportObjectPrefab, currentInteractableObject.transform.position, Quaternion.identity);
-    }
+    // public void SpawnToNextLevel(){            
+    //     Instantiate(nextLevelTeleportObjectPrefab, currentInteractableObject.transform.position, Quaternion.identity);
+    // }
 }
 
