bbd4b5c3b9ec77e64f588f17029a5eaae80f3e25
Kamal-Alfawaz <kamal.alfawaz@outlook.com>
2024-01-07 20:51:36 +0000

    Added ability images

---
+++ RogueLikeUnityFinal/Assets/Scenes/CharShowcase.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CharShowcase : MonoBehaviour
+{
+
+    public void Play(){
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Stage_1");
+    }
+
+    public void quitGame(){
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+
+}
