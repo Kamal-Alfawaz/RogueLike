c27faa67dfa71d9922502bbba9708c797def3e1c
A.J. Catak <ajcatak01@gmail.com>
2024-01-08 03:04:05 +0000

    Stage 2 Chalice Summon

---
+++ RogueLikeUnityFinal/Assets/Scripts/Interactions/BossSpawnInteractable.cs
@@ -0,0 +1,41 @@
+
+
+using UnityEngine;
+
+public class BossSpawnInteractable : MonoBehaviour, IInteractable{
+    static private int activateCount = 0;
+
+    private bool isInRange = false;
+    
+    public GameObject bossPrefab;
+    public GameObject interactionUI;
+    public Transform bossSpawnPoint;
+    private Outline outline;
+
+    void Start(){
+        outline = this.gameObject.AddComponent<Outline>();
+        outline.enabled = false;        
+    }
+    
+    public void Interact(){
+        // spawn boss
+        Debug.Log("detected");
+        activateCount++;
+        if (activateCount == 4){
+            Instantiate(bossPrefab, bossSpawnPoint.position, bossSpawnPoint.rotation);
+        }
+        this.gameObject.SetActive(false);
+    }
+
+    public void InRange(){
+        isInRange = true;
+        interactionUI.SetActive(true);
+        outline.enabled = true;    
+    }
+
+    public void OutOfRange(){
+        isInRange = false;
+        interactionUI.SetActive(false);
+        outline.enabled = false;     
+    }
+}
