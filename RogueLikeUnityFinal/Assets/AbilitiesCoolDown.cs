4da02732486176944724fb3d378d86a8e2bc2f2f
Abdi <a.mohamed@se21.qmul.ac.uk>
2024-01-08 01:50:56 +0000

    Cooldown Complete

--- RogueLikeUnityFinal/Assets/AbilitiesCoolDown.cs
+++ RogueLikeUnityFinal/Assets/AbilitiesCoolDown.cs
@@ -1,13 +1,198 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class AbilitiesCoolDown  : MonoBehaviour
+public class AbilitiesCoolDown : MonoBehaviour
 {
-    public void poop()
+    public ThirdPersonShooterController Controller;
+    [Header("Dash")]
+    public Image dashIcon;
+    public float dashCoolDown = 2;
+    bool isDashing = false;
+
+    public TextMeshProUGUI DashTimerText;
+
+    [Header("Grenade")]
+    public Image grenadeIcon;
+    public float grenadeCoolDown = 3;
+    bool isGrenading = false;
+
+    public TextMeshProUGUI GrenadeTimerText;
+
+    [Header("Double Fire")]
+    public Image doubleFireIcon;
+    public float doubleFireCoolDown = 10;
+    bool isDoubleFiring = false;
+
+    public TextMeshProUGUI DoubleFireTimerText;
+
+    [Header("AP Bullets")]
+    public Image apBulletsIcon;
+    public float apBulletsCoolDown = 0.5f;
+    bool isAPBullets = false;
+
+    public TextMeshProUGUI APBulletsTimerText;
+
+
+
+    void Start()
     {
-        Debug.Log("poop in the pants");
+        dashIcon.fillAmount = 0;
+        UpdateDashTimerText();
+
+        grenadeIcon.fillAmount = 0;
+        UpdateGrenadeTimerText();
+
+        // doubleFireIcon.fillAmount = 0;
+        // UpdateDoubleFireTimerText();
+
+        // apBulletsIcon.fillAmount = 0;
+        // UpdateAPBulletsTimerText();
     }
 
+    void Update()
+    {
+        DashIconCooldown();
+        UpdateDashTimerText();
+
+        GrenadeIconCooldown();
+        UpdateGrenadeTimerText();
+        
+        // DoubleFireIconCooldown();
+        // UpdateDoubleFireTimerText();
+
+        // APBulletsIconCooldown();
+        // UpdateAPBulletsTimerText();
+    }
+
+    public void DashIconCooldown()
+    {
+        Controller = GetComponent<ThirdPersonShooterController>();
+        if (isDashing == false && Controller.starterAssetsInputs.dash == true)
+        {
+            isDashing = true;
+            dashIcon.fillAmount = 1;
+        }
+        if (isDashing)
+        {
+            dashIcon.fillAmount -= 1 / dashCoolDown * Time.deltaTime;
+            if (dashIcon.fillAmount <= 0)
+            {
+                dashIcon.fillAmount = 0;
+                isDashing = false;
+            }
+        }
+    }
+
+    void UpdateDashTimerText()
+    {
+        if (Controller.starterAssetsInputs.dash)
+        {
+            DashTimerText.text = "";
+        }
+        else
+        {
+            float remainingTime = Mathf.Ceil(dashIcon.fillAmount * dashCoolDown);
+            DashTimerText.text = remainingTime > 0 ? remainingTime.ToString("") : "";
+        }
+    }
+
+    public void GrenadeIconCooldown()
+    {
+        Controller = GetComponent<ThirdPersonShooterController>();
+        if (isGrenading == false && Controller.starterAssetsInputs.grenade == true)
+        {
+            isGrenading = true;
+            grenadeIcon.fillAmount = 1;
+        }
+        if (isGrenading)
+        {
+            grenadeIcon.fillAmount -= 1 / grenadeCoolDown * Time.deltaTime;
+            if (grenadeIcon.fillAmount <= 0)
+            {
+                grenadeIcon.fillAmount = 0;
+                isGrenading = false;
+            }
+        }
+    }
+
+    void UpdateGrenadeTimerText()
+    {
+        if (Controller.starterAssetsInputs.grenade)
+        {
+            GrenadeTimerText.text = "";
+        }
+        else
+        {
+            float remainingTime = Mathf.Ceil(grenadeIcon.fillAmount * grenadeCoolDown);
+            GrenadeTimerText.text = remainingTime > 0 ? remainingTime.ToString("") : "";
+        }
+    }
+
+    // public void DoubleFireIconCooldown()
+    // {
+    //     Controller = GetComponent<ThirdPersonShooterController>();
+    //     if (isDoubleFiring == false && Controller.starterAssetsInputs.doubleFire == true)
+    //     {
+    //         isDoubleFiring = true;
+    //         doubleFireIcon.fillAmount = 1;
+    //     }
+    //     if (isDoubleFiring)
+    //     {
+    //         doubleFireIcon.fillAmount -= 1 / doubleFireCoolDown * Time.deltaTime;
+    //         if (doubleFireIcon.fillAmount <= 0)
+    //         {
+    //             doubleFireIcon.fillAmount = 0;
+    //             isDoubleFiring = false;
+    //         }
+    //     }
+    // }
+
+    // void UpdateDoubleFireTimerText()
+    // {
+    //     if (Controller.starterAssetsInputs.doubleFire)
+    //     {
+    //         DoubleFireTimerText.text = "";
+    //     }
+    //     else
+    //     {
+    //         float remainingTime = Mathf.Ceil(doubleFireIcon.fillAmount * doubleFireCoolDown);
+    //         DoubleFireTimerText.text = remainingTime > 0 ? remainingTime.ToString("") : "";
+    //     }
+    // }
+
+    // public void APBulletsIconCooldown()
+    // {
+    //     Controller = GetComponent<ThirdPersonShooterController>();
+    //     if (isAPBullets == false && Controller.starterAssetsInputs.apBullets == true)
+    //     {
+    //         isAPBullets = true;
+    //         apBulletsIcon.fillAmount = 1;
+    //     }
+    //     if (isAPBullets)
+    //     {
+    //         apBulletsIcon.fillAmount -= 1 / apBulletsCoolDown * Time.deltaTime;
+    //         if (apBulletsIcon.fillAmount <= 0)
+    //         {
+    //             apBulletsIcon.fillAmount = 0;
+    //             isAPBullets = false;
+    //         }
+    //     }
+    // }
+
+    // void UpdateAPBulletsTimerText()
+    // {
+    //     if (Controller.starterAssetsInputs.apBullets)
+    //     {
+    //         APBulletsTimerText.text = "";
+    //     }
+    //     else
+    //     {
+    //         float remainingTime = Mathf.Ceil(apBulletsIcon.fillAmount * apBulletsCoolDown);
+    //         APBulletsTimerText.text = remainingTime > 0 ? remainingTime.ToString("") : "";
+    //     }
+    // }
+
 }
+
+
